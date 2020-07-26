db0186082666162309460420b00bfa01e2a4d2c9
shira <shira79@users.noreply.github.com>
2020-07-27 03:39:09 +0900

    icon表示

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Vuetify from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
 import "../node_modules/vuetify/dist/vuetify.min.css";
 
 Vue.config.productionTip = false
@@ -9,6 +10,10 @@ Vue.use(Vuetify);
 
 new Vue({
   router,
-  vuetify: new Vuetify(),
+  vuetify: new Vuetify({
+      icons: {
+        iconfont: 'mdi',
+      }
+  }),
   render: h => h(App),
 }).$mount('#app')
