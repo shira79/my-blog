89f6f90d2ade0848c4bb39dc157f53059ec46cf5
shira <shira79@users.noreply.github.com>
2020-08-06 14:15:31 +0900

    tagShow実装

--- src/pages/BlogIndex.vue
+++ src/pages/BlogIndex.vue
@@ -1,13 +1,7 @@
 <template>
   <v-container class="blogindex">
     <Loading :state="loading"></Loading>
-    <v-layout column>
-      <v-col class="blog-item" v-for="item in items" :key="item.title">
-          <h3><router-link class="blog-link" :to="item.sys.id">{{item.fields.title}}</router-link></h3>
-          <Tags :tags="item.fields.tags"></Tags>
-          <PublishedAt :publishedAt="item.fields.publishedAt"></PublishedAt>
-      </v-col>
-    </v-layout>
+      <Blogs :blogs="items"></Blogs>
   </v-container>
 </template>
 
@@ -15,11 +9,10 @@
 
 import ContentfulAdapter from '../contentful.js'
 import Loading from '../components/Loading.vue'
-import Tags from '../components/Tags.vue'
-import PublishedAt from '../components/PublishedAt.vue'
+import Blogs from '../components/Blogs.vue'
 
 export default {
-    components:{ Loading, Tags, PublishedAt },
+    components:{ Loading, Blogs },
     data:function(){
       return {
         loading:false,
@@ -30,9 +23,10 @@ export default {
       setData(){
         var vm = this;
         vm.loading = true;
-        ContentfulAdapter.getBlogs()
+        ContentfulAdapter.getBlogList()
           .then(function (entry) {
             vm.items = entry.items;
+            console.log(vm.items[0]);
             vm.loading = false;
           })
       },
@@ -46,23 +40,5 @@ export default {
 <style scoped>
 .blogindex {
   max-width: 680px;
-}
-
-.blog-item {
-  border-bottom:thin solid #7f8c8d;
-  text-align: left;
-  color:#7f8c8d;
-  padding-top: 20px;
-  margin-bottom: 30px;
-}
-
-.blog-link{
-  color:black;
-  text-decoration: none;
-}
-
-.blog-link:hover {
-  color: #c1c1ff;
-  text-decoration: underline;
 }
 </style>
--- src/pages/TagShow.vue
+++ src/pages/TagShow.vue
@@ -1,44 +1,68 @@
 <template>
-  <v-container class=" tag-show">
+  <v-container class="tag-show">
       <Loading :state="loading"></Loading>
-      これから作ります
+      <h2>#{{tag.fields.jaName}}</h2>
+      <Blogs :blogs="bloglist"></Blogs>
+      <h3>全部のタグ</h3>
+      <Tags :tags="tagList"></Tags>
   </v-container>
 </template>
 
 <script>
 
-// import ContentfulAdapter from '../contentful.js'
+import ContentfulAdapter from '../contentful.js'
 import Loading from '../components/Loading.vue'
-// import Tags from '../components/Tags.vue'
+import Blogs from '../components/Blogs.vue'
+import Tags from '../components/Tags.vue'
 
 export default {
-  components: { Loading},
-//   components: { Loading, Tags, PublishedAt},
+  components: { Loading, Blogs, Tags},
   props:{ enName: String },
   data:function(){
     return {
-      loading:false,
+        tagList:[],
+        tag:[],
+        bloglist:[],
+        loading:false,
     }
   },
   methods:{
     setData(){
-      var vm = this;
-      vm.loading =  true;
-    //   ContentfulAdapter.getEntryById(this.id)
-        // .then(function (entry) {
-        //   vm.loading =  false;
-        // })
+        var vm = this;
+        vm.loading =  true;
+        ContentfulAdapter.getTagList()
+        .then(function (entry) {
+            vm.tagList = entry.items;
+        })
+        //これは非同期でいい
+
+        ContentfulAdapter.getTagByEnName(vm.enName)
+        .then(function (entry) {
+            vm.tag = entry.items[0];
+
+            ContentfulAdapter.getBlogByTagId(vm.tag.sys.id)
+            .then(function (entry) {
+                console.log(entry.items);
+                vm.bloglist = entry.items;
+                vm.loading =  false;
+            })
+        })
       }
     },
     created :function(){
       this.setData();
+    },
+    watch: {
+        $route () {
+            this.setData();
+        }
     },
   }
   </script>
 
 <style scoped>
 
-.blog-show{
+.tag-show{
     text-align:left;
     max-width:680px;
 }
